6e54f0650003ccb63a1b5c11617a449ee2ef9ae6
gavdraper <gavdraper@gmail.com>
2012-02-22 21:23:53 +0000

    Added lister to create socket for receiving responces

--- SSDPSharp/SsdpLocator.cs
+++ SSDPSharp/SsdpLocator.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 //Based on draft SSDP spec at
 //  ftp://ftp.pwg.org/pub/pwg/ipp/new_SSDP/draft-cai-ssdp-v1-03.txt
 
 namespace SSDPSharp
 {    
-    public class SsdpLocator
+    public class SsdpLocator :IDisposable
     {
-        IPAddress multicastAddress = IPAddress.Parse("239.255.255.250");
+        readonly IPAddress multicastAddress = IPAddress.Parse("239.255.255.250");
+        Socket listener;
+        bool awaitingResponse;
         const int multicastPort = 1900;
         const int unicastPort = 1901;
 
@@ -20,7 +24,7 @@ namespace SSDPSharp
         const string messageMx = "MX: 8";
         const string messageSt = "ST: ssdp:all";
 
-        string broadcastMessage =
+        readonly string broadcastMessage =
             string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{0}",
                           "/r/n",
                           messageHeader,
@@ -34,6 +38,34 @@ namespace SSDPSharp
         public SsdpLocator()
         {
             Devices = new ObservableCollection<SsdpDevice>();
+            CreateSsdpListener();
+        }
+        
+        void CreateSsdpListener()
+        {
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            listener.Bind(new IPEndPoint(IPAddress.Any, unicastPort));
+            listener.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
+                              new MulticastOption(multicastAddress, IPAddress.Any));
+            var t = new Thread(() =>
+                                   {
+                                       while (true)
+                                       {
+                                           var b = new byte[8000];
+                                           awaitingResponse = true;
+                                           listener.BeginReceive(b, 0, 8000, SocketFlags.None, new AsyncCallback(ListenerResponseReceived), null);
+                                           awaitingResponse = false;
+                                           Thread.Sleep(100);
+                                       }
+                                   });
+            t.Start();
+            
+        }
+
+        public void ListenerResponseReceived(IAsyncResult result)
+        {
+            //TODO Add code to read response
+            awaitingResponse = false;
         }
 
         public void SendSsdpIdentificationRequest()
@@ -49,5 +81,10 @@ namespace SSDPSharp
             }
         }
 
+        public void Dispose()
+        {
+            if (listener != null)
+                listener.Close();
+        }
     }
 }
