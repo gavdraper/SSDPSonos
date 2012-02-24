52d519216695fd8e1df87b01be6fd1639981b627
gavdraper <gavdraper@gmail.com>
2012-02-24 08:36:53 +0000

    Added method to receive response from socket (Doesnt work)

--- SSDPSharp/SsdpLocator.cs
+++ SSDPSharp/SsdpLocator.cs
@@ -9,12 +9,10 @@ using System.Threading;
 //  ftp://ftp.pwg.org/pub/pwg/ipp/new_SSDP/draft-cai-ssdp-v1-03.txt
 
 namespace SSDPSharp
-{    
-    public class SsdpLocator :IDisposable
+{
+    public class SsdpLocator
     {
         readonly IPAddress multicastAddress = IPAddress.Parse("239.255.255.250");
-        Socket listener;
-        bool awaitingResponse;
         const int multicastPort = 1900;
         const int unicastPort = 1901;
 
@@ -22,69 +20,61 @@ namespace SSDPSharp
         const string messageHost = "HOST: 239.255.255.250:1900";
         const string messageMan = "MAN: \"ssdp:discover\"";
         const string messageMx = "MX: 8";
-        const string messageSt = "ST: ssdp:all";
+        const string messageSt = "ST: urn:schemas-upnp-org:device:ZonePlayer:1";
 
-        readonly string broadcastMessage =
+        readonly byte[] broadcastMessage = Encoding.UTF8.GetBytes(
             string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{0}",
-                          "/r/n",
+                          "\r\n",
                           messageHeader,
                           messageHost,
                           messageMan,
                           messageMx,
-                          messageSt);
+                          messageSt));
 
         public ObservableCollection<SsdpDevice> Devices { get; set; }
 
         public SsdpLocator()
         {
             Devices = new ObservableCollection<SsdpDevice>();
-            CreateSsdpListener();
-        }
-        
-        void CreateSsdpListener()
-        {
-            listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            listener.Bind(new IPEndPoint(IPAddress.Any, unicastPort));
-            listener.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
-                              new MulticastOption(multicastAddress, IPAddress.Any));
-            var t = new Thread(() =>
-                                   {
-                                       while (true)
-                                       {
-                                           var b = new byte[8000];
-                                           awaitingResponse = true;
-                                           listener.BeginReceive(b, 0, 8000, SocketFlags.None, new AsyncCallback(ListenerResponseReceived), null);
-                                           awaitingResponse = false;
-                                           Thread.Sleep(100);
-                                       }
-                                   });
-            t.Start();
-            
         }
 
-        public void ListenerResponseReceived(IAsyncResult result)
+        public void CreateSsdpListener()
         {
-            //TODO Add code to read response
-            awaitingResponse = false;
-        }
-
-        public void SendSsdpIdentificationRequest()
-        {
-            using (var identificationRequest = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
             {
-                identificationRequest.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,new MulticastOption(multicastAddress));
-                identificationRequest.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
-                identificationRequest.Connect(new IPEndPoint(multicastAddress, multicastPort));
-                var message = Encoding.UTF8.GetBytes(broadcastMessage);
-                identificationRequest.Send(message, message.Length, SocketFlags.None);
-                identificationRequest.Close();
+                socket.Bind(new IPEndPoint(IPAddress.Any, unicastPort));
+                socket.Connect(new IPEndPoint(multicastAddress, multicastPort));
+                var thd = new Thread(()=>GetSocketResponse(socket));                
+                socket.Send(broadcastMessage, 0, broadcastMessage.Length, SocketFlags.None);
+                thd.Start();
+                Thread.Sleep(20000);
+                socket.Close();
             }
         }
 
-        public void Dispose()
+        public void GetSocketResponse(Socket socket)
         {
-            if (listener != null)
-                listener.Close();
+                try
+                {
+                    while (true)
+                    {
+                        var response = new byte[8000];
+                        EndPoint ep = new IPEndPoint(IPAddress.Any, unicastPort);
+                        socket.ReceiveFrom(response, ref ep);
+                        var str = Encoding.UTF8.GetString(response);
+                        Devices.Add(new SsdpDevice() { Location = str });
+                    }
+                }
+                catch
+                {
+                    //TODO handle exception for when connection closes
+                }
+
         }
+
+
+
+
+
     }
 }
--- TestConsole/SsdpConsole.cs
+++ TestConsole/SsdpConsole.cs
@@ -13,9 +13,51 @@ namespace TestConsole
         static void Main()
         {
             locator = new SsdpLocator();
-            locator.Devices.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(DevicesCollectionChanged);
-            locator.SendSsdpIdentificationRequest();
+            //locator.Devices.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(DevicesCollectionChanged);
+            //locator.SendSsdpIdentificationRequest();
+            ShowMenu();
+        }
+
+        static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1:Find New Sonos Devices");
+            Console.WriteLine("2:List Discovered Sonos Devices");
+            Console.WriteLine("3:Send Identification Request");
+            var menuSelection = int.Parse(Console.ReadLine());
+            switch (menuSelection)
+            {
+                case 1:
+                    FindNewSonosDevices();
+                    break;
+                case 2:
+                    ListDiscoveredSonosDevices();
+                    break;
+            }
+        }
+
+
+        static void FindNewSonosDevices()
+        {
+            Console.Clear();
+            Console.WriteLine("Hit The Sync Buttons On The Sonos Device You Want To Discover Then Press Enter");
             Console.ReadLine();
+            locator.CreateSsdpListener();            
+            ShowMenu();
+        }
+
+        private static void ListDiscoveredSonosDevices()
+        {
+            Console.Clear();
+            if(locator.Devices.Count==0)
+                Console.WriteLine("No Devices Found");
+            else
+            {
+                foreach(var d in locator.Devices)
+                    Console.WriteLine(d.Location);
+            }
+            Console.ReadLine();
+            ShowMenu();
         }
 
         static void DevicesCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
